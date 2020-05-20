e2df7810d292c080db15b5565e8738811074a565
karinadesyatnik <karinadesyatnik27@gmail.com>
2020-05-21 00:41:17 +0300

    vh for chrome

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
  document.getElementById('video-btn').onclick = function() {
   document.getElementById('wistia_responsive_padding').classList.add('active');
 }
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
