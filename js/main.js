04db1a56cbc8e4de9116573c2749b8b4d3d2f1f9
karinadesyatnik <karinadesyatnik27@gmail.com>
2020-05-21 01:11:00 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
- document.getElementById('video-btn').onclick = function() {
-  document.getElementById('wistia_responsive_padding').classList.add('active');
-}
-
-
 
 window.addEventListener('resize', () => {
 
